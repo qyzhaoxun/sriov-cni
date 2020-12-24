9014fe5ba588eccd394faae2831bdf041b04757c
xunqyzhao <xunqyzhao@tencent.com>
2020-12-24 16:21:56 +0800

    chore: update .gitignore and build.sh

--- build.sh
+++ build.sh
@@ -6,15 +6,6 @@ if [ "$(uname)" == "Darwin" ]; then
 	export GOOS="${GOOS:-linux}"
 fi
 
-ORG_PATH="github.com/Mellanox"
-export REPO_PATH="${ORG_PATH}/sriov-cni"
-
-if [ ! -h gopath/src/${REPO_PATH} ]; then
-	mkdir -p gopath/src/${ORG_PATH}
-	ln -s ../../../../ gopath/src/${REPO_PATH} || exit 255
-fi
-
-export GOPATH=${PWD}/gopath
 export GO="${GO:-go}"
 
 mkdir -p "${PWD}/bin"
@@ -34,7 +25,7 @@ for d in $PLUGINS; do
 			fi
 		else
 			echo "  $plugin"
-		        GOARCH=$ARCH $GO build -ldflags "${LD_FLAGS}" -o "${PWD}/bin/${plugin}" "${@}" "${REPO_PATH}"/$d
+		        GOARCH=$ARCH $GO build -ldflags "${LD_FLAGS}" -o "${PWD}/bin/${plugin}" "${@}" ./$d
 		fi
 	fi
 done
