6e20ce051422a638208abea91d02ec86f5878796
xunqyzhao <xunqyzhao@tencent.com>
2020-12-25 16:24:19 +0800

    feat: add support for rdma cni

--- k8s-installer/installer.sh
+++ k8s-installer/installer.sh
@@ -1,13 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 
-if [ ! -e /host-cni-bin/sriov ]; then
-	cp /bin/sriov /host-cni-bin/
+cp -f /rdma /host-cni-bin/
+
+cp -f /sriov /host-cni-bin/
+
+if [ ! -e /host-cni-etc/20-tke-sriov-rdma.conflist ]; then
+	cp /20-tke-sriov-rdma.conflist /host-cni-etc/
 fi
 
-if [ ! -e /host-cni-bin/fixipam ]; then
-	cp -f /bin/fixipam /host-cni-bin/
-fi
-
-if [ ! -e /host-cni-etc/10-sriov-cni.conf ]; then
-	cp /installer/10-sriov-cni.conf /host-cni-etc/
-fi
+while sleep 3600; do :; done
